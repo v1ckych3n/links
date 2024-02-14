662cda7021590e84b2db9d80e91d0a1098aee814
v1ckych3n <chenv159@newschool.edu>
2024-02-13 20:12:07 -0500

    getting arena connected

--- assets/arena.js
+++ assets/arena.js
@@ -1,3 +1,9 @@
+// This allows us to process/render the descriptions, which are in Markdown!
+// More about Markdown: https://en.wikipedia.org/wiki/Markdown
+let markdownIt = document.createElement('script')
+markdownIt.src = 'https://cdn.jsdelivr.net/npm/markdown-it@14.0.0/dist/markdown-it.min.js'
+document.head.appendChild(markdownIt)
+
 // Okay, Are.na stuff!
 let channelSlug = 'dystopia-szw6q7kh94s' // The “slug” is just the end of the URL
 
